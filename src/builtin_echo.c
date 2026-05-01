68eb6cb0cdb1753ff367b5ba0370c08f938811db
Camila Pedraza <36695574+camilapedraza@users.noreply.github.com>
2026-05-01 22:43:26 +0200

    feat: builtin echo function working

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,8 +6,57 @@
 /*   By: mpedraza <mpedraza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 18:30:42 by mpedraza          #+#    #+#             */
-/*   Updated: 2026/04/27 22:15:37 by mpedraza         ###   ########.fr       */
+/*   Updated: 2026/05/01 22:31:06 by mpedraza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+bool	is_n_flag(char *field)
+{
+	int	index;
+
+	index = 0;
+	if (field[index] != CHAR_DASH)
+		return (false);
+	index++;
+	if (!field[index])
+		return (false);
+	while (field[index])
+	{
+		if (field[index++] != CHAR_N)
+			return (false);
+	}
+	return (true);
+}
+
+// THIS WILL PASS AN EXIT CODE TO THE CHILD SO IT NEEDS C EXIT CODES!
+// assume fields starts at cmd->argv[1], so index here should be 0
+int	builtin_echo(char **fields)
+{
+	bool	newline;
+	int		index;
+
+	if (!fields || !*fields)
+	{
+		ft_putchar_fd(CHAR_NEWLINE, STDOUT_FILENO);
+		return (EXIT_SUCCESS);
+	}
+	newline = true;
+	index = 0;
+	while (fields[index] && is_n_flag(fields[index]))
+	{
+		newline = false;
+		index++;
+	}
+	while (fields[index])
+	{
+		ft_putstr_fd(fields[index], STDOUT_FILENO);
+		index++;
+		if (fields[index])
+			ft_putchar_fd(CHAR_SPACE, STDOUT_FILENO);
+	}
+	if (newline)
+		ft_putchar_fd(CHAR_NEWLINE, STDOUT_FILENO);
+	return (EXIT_SUCCESS);
+}
