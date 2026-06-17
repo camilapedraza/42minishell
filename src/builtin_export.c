efb1d053d2b6abb262218afec51d12e840f1266d
plepercq <plepercq@f6r4s14.paris.42.school>
2026-06-17 18:38:37 +0200

    add export

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,36 +6,75 @@
 /*   By: plepercq <plepercq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 18:30:12 by plepercq          #+#    #+#             */
-/*   Updated: 2026/06/16 15:53:08 by plepercq         ###   ########.fr       */
+/*   Updated: 2026/06/17 18:36:48 by plepercq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_env	*parse_var(char *envp)
+bool	is_var_key_valid(char *key)
+{
+	int	i;
+	int	klen;
+
+	if (!key)
+		return (false);
+	if (!isalpha(key[0]))
+		return (false);
+	i = 1;
+	klen = strlen(key);
+	while (i < klen)
+	{
+		if (!isalnum(key[i]) && !(key[i] != '_'))
+			return (false);
+	}
+	return (true);
+}
+
+static t_env	*parse_var(char *var)
 {
 	char	*div;
 	char	*key;
 	char	*value;
 	t_env	*var;
 
-	div = ft_strchr(envp, CHAR_EQUALS);
+	div = ft_strchr(var, CHAR_EQUALS);
 	if (div)
 	{
-		key = ft_substr(envp, 0, div - envp);
+		key = ft_substr(var, 0, div - var);
 		value = ft_strdup(div + 1);
 	}
 	else
 	{
-		key = ft_strdup(envp);
-		value = ft_calloc(1, 1);
-		value[0] = '\0';
+		key = ft_strdup(var);
+		value = NULL;
 	}
-	var = new_var(key, value);
+	if (is_var_key_valid(key))
+		return (new_var(key, value));
 	if (key)
 		free(key);
 	if (value)
 		free(value);
+	return (var);
+}
+
+t_env	*new_var(char *key, char *value)
+{
+	t_env	*var;
+
+	if (!key || !value)
+		return (NULL);
+	var = malloc(sizeof(t_env));
+	if (!var)
+		return (NULL);
+	var->key = ft_strdup(key);
+	var->value = ft_strdup(value);
+	if (!var->key || !var->value)
+	{
+		free(var);
+		return (NULL);
+	}
+	var->next = NULL;
 	return (var);
 }
 
